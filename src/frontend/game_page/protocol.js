f9b91502d7ef84c3f3d9bdb22689efe34772e298
Lucas Straub <lucas.straub2003@gmail.com>
2020-04-23 15:17:06 +0200

    client side protocol added
    not working yet

---
+++ src/frontend/game_page/protocol.js
@@ -0,0 +1,47 @@
+class Protocol {
+    domain;
+    connection;
+    
+    constructor() {
+        this.domain = window.location.hostname;
+        this.connection = new WebSocket(`ws://${this.domain}/backend/protocol.php`, []);
+
+        this.connection.onerror = function (error) {
+            sendEvent('error');
+        };
+    }
+
+    //---------------------------
+    // Methods
+    //---------------------------
+    setUsername(username) {
+        var message = { type: username, name: username };
+        this.connection.send(message);
+    }
+
+    //---------------------------
+    // Events
+    // error
+    //---------------------------
+    events = new function() {
+        var _triggers = {};
+      
+        this.on = function(event,callback) {
+            if(!_triggers[event])
+                _triggers[event] = [];
+            _triggers[event].push( callback );
+          }
+      
+        this.triggerHandler = function(event,params) {
+            if( _triggers[event] ) {
+                var i;
+                for( i in _triggers[event] )
+                    _triggers[event][i](params);
+            }
+        }
+    };
+
+    sendEvent(eventText) {
+        this.events.triggerHandler(eventText);
+    }
+}
