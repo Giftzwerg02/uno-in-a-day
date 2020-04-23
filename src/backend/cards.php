73923c75ab7a4412724fd8cae6a676effbb28a1f
Unknown <chessplayer1@gmx.at>
2020-04-23 14:50:14 +0200

    Added some more functionality for card_ids

--- src/backend/cards.php
+++ src/backend/cards.php
@@ -1,26 +1,49 @@
 <?php
 
+class Cards {
 
-$colored_card_colors = ["red", "blue", "green", "yellow"];
-$colored_card_names = ["zero", "one", "two", "three". "four", "five", "six", "seven", "eight", "nine", "blocked", "switch", "plus_two"];
-$black_card_color = "black";
-$black_card_names = ["plus_four", "color_change"];
-$card_ids = [];
+    private $colored_card_colors = ["red", "blue", "green", "yellow"];
+    private $colored_card_names = ["zero", "one", "two", "three". "four", "five", "six", "seven", "eight", "nine", "blocked", "switch", "plus_two"];
+    private $black_card_color = "black";
+    private $black_card_names = ["plus_four", "color_change"];
+    public $card_ids = [];
 
-// ADDING COLOR-CARDS
-for ($color=0; $color < count($colored_card_colors); $color++) { 
-    
-    for ($name=0; $name < count($colored_card_names); $name++) { 
-        
-        $card_ids[] = $colored_card_colors[$color] . "/" . $colored_card_names[$name];
-
+    function __construct() {
+        $this->generateCards();
     }
 
-}
+    private function generateCards() {
 
-// ADDING BLACK CARDS
-for ($name=0; $name < count($black_card_names); $name++) { 
-    $card_ids[] = $black_card_color . "/" . $black_card_names[$name];
+        // ADDING COLOR-CARDS
+        for ($color=0; $color < count($this->colored_card_colors); $color++) { 
+                
+            for ($name=0; $name < count($this->colored_card_names); $name++) { 
+                
+                $this->card_ids[] = $this->colored_card_colors[$color] . "/" . $this->colored_card_names[$name];
+
+            }
+
+        }
+
+        // ADDING BLACK CARDS
+        for ($name=0; $name < count($this->black_card_names); $name++) { 
+            $this->card_ids[] = $this->black_card_color . "/" . $this->black_card_names[$name];
+        }
+
+    }
+    
+    public function getCardColor($card_id) {
+        return explode("/", $card_id)[0];
+    }
+
+    public function getCardName($card_id) {
+        return explode("/", $card_id)[1];
+    }
+
+    public function getCardFile($card_id) {
+        return $card_id . ".png";
+    }
+
 }
 
 ?>
--- src/backend/index.php
+++ src/backend/index.php
@@ -1,7 +1,5 @@
 <?php 
 
 require "cards.php";
-global $card_ids;
-
 
 ?>
