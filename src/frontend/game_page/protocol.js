546db67eb9eddd2093c0b6d116224dff3cf5d3b9
unknown <chessplayer1@gmx.at>
2020-04-23 19:00:04 +0200

    Added protocol-functions to protocol.js and added user-deck graphically

--- src/frontend/game_page/protocol.js
+++ src/frontend/game_page/protocol.js
@@ -5,9 +5,78 @@ class Protocol {
     //---------------------------
     // Methods
     //---------------------------
-    setUsername(username) {
-        var message = { type: username, name: username };
-        this.connection.send(message);
+    sessionInit(username) {
+        var message = { type: "session_init", name: username };
+        this.connection.send(JSON.stringify(message));
+    }
+
+    error(errorMessage) {
+        var message = { type: "error", message: errorMessage };
+        this.connection.send(JSON.stringify(message));
+    }
+
+    gameStart(names, firstCard) {
+        var message = { type: "game_start", names: names, first_card: firstCard };
+        this.connection.send(JSON.stringify(message));
+    }
+
+    gameEnd(winner) {
+        var message = { type: "game_end", winner: winner };
+        this.connection.send(JSON.stringify(message));
+    }
+
+    requestCard() {
+        var message = { type: "request_card" };
+        this.connection.send(JSON.stringify(message));
+    }
+
+    giveCard(card) {
+        var message = { type: "give_card", card: card };
+        this.connection.send(JSON.stringify(message));
+    }
+
+    pushCard(card) {
+        var message = {type: "push_card", card: card}
+        this.connection.send(JSON.stringify(message));
+    }
+
+    updateUser(name, cards) {
+        var message = { type: "update_user", name: name, cards: cards };
+        this.connection.send(JSON.stringify(message));
+    }
+
+    updateCurrentUser(name) {
+        var message = { type: "update_current_user", name: name };
+        this.connection.send(JSON.stringify(message));
+    }
+
+    disconnectUser(name) {
+        var message = { type: "disconnect_user", name: name };
+        this.connection.send(JSON.stringify(message));
+    }
+
+    endTurn() {
+        var message = { type: "end_turn" };
+        this.connection.send(JSON.stringify(message));
+    }
+    
+    updateTos(card) {
+        var message = { type: "update_tos", card: card };
+        this.connection.send(JSON.stringify(message));
+    }
+    
+    uno() {
+        var message = { type: "uno" };
+        this.connection.send(JSON.stringify(message));
+    }
+
+    noU() {
+        var message = { type: "no_u" };
+        this.connection.send(JSON.stringify(message));
+    }
+    
+    echo(message) {
+        console.log("echo echo echo " + message);
     }
 
     //---------------------------
@@ -41,10 +110,11 @@ class Protocol {
 
     constructor() {
         this.domain = window.location.hostname;
-        this.connection = new WebSocket(`ws://${this.domain}/backend/protocol.php`, []);
+        this.connection = new WebSocket(`ws://${this.domain}:1337/backend/protocol.php`, []);
 
+        const protocol = this;
         this.connection.onerror = function (error) {
-            this.sendEvent('error');
+            protocol.sendEvent('error');
         };
     }
 }
